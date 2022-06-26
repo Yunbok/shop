eea35c28007543deec5865682369d27a6b231558
배윤복 <qodbsqhr@naver.com>
2022-06-26 22:50:40 +0900

    thymeleaf 기본예제

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -21,6 +21,7 @@ public class ThymeleafExController {
         itemDto.setItemDetail("상품 상세 설명");
         itemDto.setItemNm("테스트 상품1");
         itemDto.setPrice(10000);
+
         itemDto.setRegTime(LocalDateTime.now());
 
         model.addAttribute("itemDto", itemDto);
