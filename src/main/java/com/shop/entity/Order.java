a5ce98b476fedd0ff03edce91e4909d971eea1e1
배윤복 <qodbsqhr@naver.com>
2022-06-30 23:59:38 +0900

    주문 entity 추가 및 조인 설정

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,40 @@
+package com.shop.entity;
+
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderData; //주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문 상태
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
